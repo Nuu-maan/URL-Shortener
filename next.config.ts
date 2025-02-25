7f7b326c5903d27c59c7f2a56e48b3f578389855
nuu-maan <n82238895@gmail.com>
2025-02-25 20:12:16 +0530

    changes in next config #17

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  ignoreBuildErrors: true,
-  /* config options here */
+  typescript: {
+    // !! WARNING !!
+    // This allows production builds to successfully complete even if
+    // your project has type errors. Use with caution.
+    ignoreBuildErrors: true,
+  },
+  // Add other Next.js config options here
 };
 
 export default nextConfig;
